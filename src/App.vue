967f6f87d26bd18063cb760606ba868b8e3c666f
krava9999 <kosmo@internet.ru>
2021-11-01 18:40:36 +1000

    калькулятор v.2

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="appProject">
+    <Calc/>
+  </div>
+</template>
+
+<script>
+import Calc from './components/calc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calc
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
